c7c840578cbcc7a141146f8b818579468602c731
jehad2002 <158752295+jehad2002@users.noreply.github.com>
2025-02-05 15:58:25 +0200

    Add files via upload

---
+++ math-skills/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func readData(filePath string) ([]float64, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var data []float64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		value, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, value)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func calculateStats(data []float64) (int, int, int, int) {
+	var sum float64
+	for _, value := range data {
+		sum += value
+	}
+
+	average := int(math.Round(sum / float64(len(data))))
+
+	sortedData := make([]float64, len(data))
+	copy(sortedData, data)
+	sort.Float64s(sortedData)
+
+	var median float64
+	if len(sortedData)%2 == 0 {
+		median = (sortedData[len(sortedData)/2-1] + sortedData[len(sortedData)/2]) / 2
+	} else {
+		median = sortedData[len(sortedData)/2]
+	}
+	medianInt := int(math.Round(median))
+
+	var variance float64
+	for _, value := range data {
+		variance += math.Pow(value-float64(average), 2)
+	}
+
+	variance = variance / float64(len(data))
+	varianceInt := int(math.Round(variance))
+	stdDev := int(math.Round(math.Sqrt(variance)))
+
+	return average, medianInt, varianceInt, stdDev
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run . data.txt")
+		os.Exit(1)
+	}
+
+	filePath := os.Args[1]
+	data, err := readData(filePath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+
+	avg, median, variance, stdDev := calculateStats(data)
+	fmt.Println("Average:", avg)
+	fmt.Println("Median:", median)
+	fmt.Println("Variance:", variance)
+	fmt.Println("Standard Deviation:", stdDev)
+}
